0dc76580deca86a1f8e4fbf7017fabf400611e92
sanjanasanju6 <sanjanaale36@gmail.com>
2025-01-03 12:35:34 +0530

    first commit

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
 
-const LandingPage = () => {
-    return (
-        <div className="container text-center mt-5">
-            <div className="jumbotron bg-light p-5">
-                <h1 className="display-4">FusionWork Collab</h1>
-                <p className="lead">
-                    FusionWork Collab is go-to platform for seamless real-time collaboration. 
-                    
-                    Work together on documents, share ideas, and communicate effortlessly with your team.
-                </p>
-                <hr className="my-4" />
-                
-                <div className="mt-4">
-                    <Link to="/register" className="btn btn-primary btn-lg me-3">Register</Link>
-                    <Link to="/login" className="btn btn-danger btn-lg">Login</Link>
-                </div>
-            </div>
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <div className="logo-container">
+          <img src="https://images-platform.99static.com//bAxzB300pGAVOoAalSIE07qDIKI=/0x0:2000x2000/fit-in/500x500/projects-files/76/7697/769781/ba912989-25a6-489c-85c6-5204fec2416d.jpg" alt="FusionWork Logo" className="logo" />
         </div>
-    );
-};
+        <h1 className="title">Welcome to <span>FusionWork</span></h1>
+        <p className="tagline">Create • Collaborate • Conquer</p>
+        <div className="button-group">
+          <Link to="/login">
+            <button className="btn btn-primary">Login</button>
+          </Link>
+          <Link to="/register">
+            <button className="btn btn-secondary">Register</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default LandingPage;
+
